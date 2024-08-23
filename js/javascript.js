770ef1374ed9d02cb73331b2b8fed5f2f2dcdf98
misterclock <emanoellorenzzutti@hotmail.com>
2024-08-23 11:23:28 -0300

    Logic to deal with user`s choices
    A function was created to deal with user`s choices in the program.

--- js/javascript.js
+++ js/javascript.js
@@ -1,8 +1,27 @@
 //Create a Rock-Paper-Scissors game using the console as a project
 
-//Create a way of receiving a guess from the user as Rock, Paper or scissor.
+
 
 //Compare the function to compare the results from the computer and the user
+
+//Create a function to receive a choice from the user as Rock, Paper or scissor.
+let userChoice = prompt("Teste");
+let userAnswer = getHumanChoice(userChoice);
+console.log(userAnswer);
+
+function getHumanChoice(choice){
+    switch (choice){
+        case `1`:
+            return "ROCK";
+        
+        case `2`:
+            return "PAPER";
+
+        case `3`:
+            return "SCISSOR";
+    }
+
+}
 
 //Create a function that will return rock, paper or scissor with a random frequency as the computer
 function getComputerChoice (){
