3450803b46d8df35724672a978ac0d996bbe76d4
misterclock <emanoellorenzzutti@hotmail.com>
2024-09-11 15:44:50 -0300

    Creation of user interaction + creation of rounds

--- js/javascript.js
+++ js/javascript.js
@@ -1,25 +1,78 @@
 //Create a Rock-Paper-Scissors game using the console as a project
+let humanScore = 0;
+console.log("The human score is " + humanScore.toString());
+let computerScore = 0;
+console.log("The computer socre is " + computerScore.toString());
 
-
+let choiceHuman = prompt("Please, insert your choice for the Rock Paper Scissors", "PAPER");
+let funHumanChoice = getHumanChoice(choiceHuman);
+let funComputerChoice = getComputerChoice();
+console.log("The computer choice is " + funComputerChoice);
+let roundResult = playRound(funHumanChoice, funComputerChoice);
+console.log(roundResult);
+console.log("Now the human score is " + humanScore + " and the computer socre is " + computerScore)
 
 //Compare the function to compare the results from the computer and the user
+function playRound(humanChoice, computerChoice){
+    if (computerChoice == "ROCK"){
+        switch (humanChoice){
+            case "ROCK":
+                return "It's a tie!";
+                break;
+            
+            case "PAPER":
+                return "You win! PAPER beats ROCK";
+                humanScore++;
+                break;
+
+            case "SCISSOR":
+                return "You lose! ROCK beats SCISSOR";
+                computerScore++;
+                break;
+        }
+    }
+
+    if (computerChoice == "PAPER"){
+        switch (humanChoice){
+            case "ROCK":
+                return "You lose! PAPER beats ROCK";
+                computerScore++;
+                break;
+            
+            case "PAPER":
+                return "It`s a tie!";
+                break;
+
+            case "SCISSOR":
+                return "You win! SCISSOR beats PAPER";
+                humanScore++;
+                break;
+        }
+    }
+
+    if (computerChoice == "SCISSOR"){
+        switch (humanChoice){
+            case "ROCK":
+                return "You win! ROCK beats SCISSOR";
+                humanScore++;
+                break;
+            
+            case "PAPER":
+                return "You lose! SCISSOR beats PAPER";
+                computerScore++;
+                break;
+
+            case "SCISSOR":
+                return "It's a tie!";
+                break;
+        }
+    }
+
+}
 
 //Create a function to receive a choice from the user as Rock, Paper or scissor.
-let userChoice = prompt("Teste");
-let userAnswer = getHumanChoice(userChoice);
-console.log(userAnswer);
-
 function getHumanChoice(choice){
-    switch (choice){
-        case `1`:
-            return "ROCK";
-        
-        case `2`:
-            return "PAPER";
-
-        case `3`:
-            return "SCISSOR";
-    }
+    return choice.toUpperCase();
 
 }
 
@@ -37,7 +90,3 @@ function getComputerChoice (){
 
     }
 }
-
-
-
-console.log("Hello")
